67bf66e02615b07f83418b95f1d14d64a7ef2804
Antti Kaihola <akaihol+django@ambitone.com>
2012-09-28 16:43:36 +0300

    Bumped 2General form version to 1.11-g2.4

--- setup.py
+++ setup.py
@@ -8,7 +8,7 @@ except:
     pass
 
 setup(name='django-mediagenerator',
-      version='1.11-g2.3.1',
+      version='1.11-g2.4',
       packages=find_packages(exclude=('tests', 'tests.*',
                                       'base_project', 'base_project.*')),
       package_data={'mediagenerator.filters': ['pyjslibs/*.py', '*.rb'],
